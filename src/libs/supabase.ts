1da9d13319690ea3352eb08886986cb5534cea01
Amir Azimi <azymy3354@gmail.com>
2025-07-08 15:11:33 +0330

    add feature: create-user-trigger; fix bug: local-supabase-connection

--- src/libs/supabase.ts
+++ src/libs/supabase.ts
@@ -5,11 +5,12 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string;
 
+
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
     auth: {
         storage: AsyncStorage,
         autoRefreshToken: true,
         persistSession: true,
-        detectSessionInUrl: false,
+        detectSessionInUrl: false
     },
 })
