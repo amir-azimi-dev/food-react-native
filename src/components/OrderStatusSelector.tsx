786f735fbd5f76143dd75e08cc8c8ee1bf5ef8ad
Amir Azimi <azymy3354@gmail.com>
2025-07-12 20:06:58 +0330

    add feature: noitfy-user when order-status changed (push notification)

--- src/components/OrderStatusSelector.tsx
+++ src/components/OrderStatusSelector.tsx
@@ -3,14 +3,15 @@ import Colors from "@/constants/Colors"
 import { OrderStatus, Tables } from "@/types";
 import useEditOrderStatus from "@/hooks/orders/useEditOrderStatus";
 import { useState } from "react";
+import { notifyUserAboutOrderStatusUpdate } from "@/libs/notifications";
 
 const orderStatusList: OrderStatus[] = ["New", "Cooking", "Delivering", "Delivered"];
 
-const OrderStatusSelector = ({ id: orderId, status: orderStatus }: Tables<"orders">) => {
+const OrderStatusSelector = ({ id: orderId, status: orderStatus, user_id }: Tables<"orders">) => {
     const [isMutating, setIsMutating] = useState<boolean>(false);
     const { mutate: editOrderStatus } = useEditOrderStatus();
 
-    const updateOrderStatusHandler = (newStatus: OrderStatus) => {
+    const updateOrderStatusHandler = (newStatus: OrderStatus): void => {
         if (isMutating) return;
 
         setIsMutating(true);
@@ -21,6 +22,7 @@ const OrderStatusSelector = ({ id: orderId, status: orderStatus }: Tables<"order
                 onSuccess: () => {
                     alert("Order status updated successfully.");
                     setIsMutating(false);
+                    notifyUserAboutOrderStatusUpdate(user_id, newStatus);
                 },
                 onError: () => {
                     setIsMutating(false);
--- src/libs/notifications.ts
+++ src/libs/notifications.ts
@@ -2,6 +2,7 @@ import { Platform } from "react-native";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
 import Constants from "expo-constants";
+import { supabase } from "./supabase";
 
 Notifications.setNotificationHandler({
     handleNotification: async () => ({
@@ -13,23 +14,23 @@ Notifications.setNotificationHandler({
 });
 
 
-export async function sendPushNotification(expoPushToken: string) {
+export async function sendPushNotification(expoPushToken: string, title: string, body: string) {
     if (!expoPushToken) return alert("Must use a device with Push Notifications enabled!");
 
     const message = {
         to: expoPushToken,
-        sound: 'default',
-        title: 'Original Title',
-        body: 'And here is the body!',
-        data: { someData: 'goes here' },
+        sound: "default",
+        title,
+        body,
+        data: { someData: "goes here" },
     };
 
-    const result = await fetch('https://exp.host/--/api/v2/push/send', {
-        method: 'POST',
+    await fetch("https://exp.host/--/api/v2/push/send", {
+        method: "POST",
         headers: {
-            Accept: 'application/json',
-            'Accept-encoding': 'gzip, deflate',
-            'Content-Type': 'application/json',
+            Accept: "application/json",
+            "Accept-encoding": "gzip, deflate",
+            "Content-Type": "application/json",
         },
         body: JSON.stringify(message),
     });
@@ -79,4 +80,13 @@ export async function registerForPushNotificationsAsync() {
 function handleRegistrationError(errorMessage: string) {
     alert(errorMessage);
     throw new Error(errorMessage);
+}
+
+export async function notifyUserAboutOrderStatusUpdate(userId: string, newStatus: string) {
+    const { data, error } = await supabase.from("profiles").select("expo_push_token").eq("id", userId).single();
+    if (error) throw new Error(error.message);
+
+    if (!data?.expo_push_token) return;
+
+    await sendPushNotification(data.expo_push_token, "Order Status Update", `Your order has been updated to "${newStatus}"`);
 }
